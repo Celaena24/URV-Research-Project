8317f2065137650dc1e8ab9dd018645ffbb18f10
Stella Dey <stelladey24@gmail.com>
2024-06-30 15:00:19 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Ad Rotator</h1>
+        <h1>Movie Recommendations</h1>
       </header>
       <main>
         {isLoading ? (
